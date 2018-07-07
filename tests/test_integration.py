a7093d74a238b7814b2cf18f422c6458ceb9569b
Gerrit Ansmann <bla@example.com>
2018-07-07 10:57:40 +0200

    Testing multi-events.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,6 @@
 from oogillespie import Gillespie
 from math import sqrt
-from pytest import approx, fixture
+from pytest import approx, fixture, mark
 
 def binomial_approx(n,p):
 	return approx(n*p,abs=3*sqrt(n*p*(1-p)))
@@ -8,7 +8,7 @@ def binomial_approx(n,p):
 def poisson_approx(t,λ):
 	return approx(t*λ,3*sqrt(λ))
 
-rates = (1,2,3)
+rates = (1,2,3,0)
 max_time = 2e3
 
 class SimpleProcess(Gillespie):
@@ -40,10 +40,47 @@ class SimpleProcess(Gillespie):
 	def state(self):
 		return self.a, self.b, self.c
 
+class MultiEventProcess(Gillespie):
+	def initialise(self):
+		self.N = [0,0,0]
+	
+	@Gillespie.event(rates)
+	def increase(self,i):
+		if i<3:
+			self.N[i] += 1
+		else:
+			raise AssertionError("This event should never happen")
+	
+	def state(self):
+		return self.N
+
+class VariableRateMultiEventProcess(Gillespie):
+	def initialise(self):
+		self.N = [0,0,0]
+	
+	@Gillespie.event
+	def increase(self,i):
+		if i<3:
+			self.N[i] += 1
+		else:
+			raise AssertionError("This event should never happen")
+	
+	@increase.rate
+	def increase_rate(self):
+		return rates
+	
+	def state(self):
+		return self.N
+
+@mark.parametrize("Process",[
+		SimpleProcess,
+		MultiEventProcess,
+		VariableRateMultiEventProcess,
+	])
 class TestIntegration(object):
 	@fixture
-	def process(self):
-		proc = SimpleProcess(max_t=max_time,max_steps=1e10)
+	def process(self,Process):
+		proc = Process(max_t=max_time,max_steps=1e10)
 		for _ in proc:
 			pass
 		return proc
@@ -58,13 +95,13 @@ class TestIntegration(object):
 	def test_time(self,process):
 		assert process.time < max_time
 		assert process.time > max_time - 10/sum(rates)
-
-def test_seeds():
-	seed = 42
-	run_1 = list(SimpleProcess(seed=42))
-	run_2 = list(SimpleProcess(seed=42))
-	run_3 = list(SimpleProcess(seed=23))
 	
-	assert run_1 == run_2
-	assert run_3 != run_1
+	def test_seeds(self,Process):
+		seed = 42
+		run_1 = list(Process(seed=42))
+		run_2 = list(Process(seed=42))
+		run_3 = list(Process(seed=23))
+		
+		assert run_1 == run_2
+		assert run_3 != run_1
 
