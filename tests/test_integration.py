dd851e31a8c8493fe04d6bf64b44d17ac025d1a7
Gerrit Ansmann <bla@example.com>
2018-07-07 12:21:21 +0200

    Base class for test processes; testing fixed-rate 2D-multi-events.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -12,53 +12,52 @@ def poisson_approx(t,λ):
 rates = (1,2,3,0)
 max_time = 2e3
 
-class SimpleProcess(Gillespie):
+class ProcessBase(Gillespie):
 	def initialise(self):
-		self.a = 0
-		self.b = 0
-		self.c = 0
+		self.N = [0,0,0]
 	
+	def state(self):
+		return self.N
+
+class SimpleProcess(ProcessBase):
 	@Gillespie.event(rates[0])
-	def increase_a(self):
-		self.a += 1
+	def increase_N0(self):
+		self.N[0] += 1
 	
 	@Gillespie.event
-	def increase_b(self):
-		self.b += 1
+	def increase_N1(self):
+		self.N[1] += 1
 	
-	@increase_b.rate
-	def b_rate(self):
+	@increase_N1.rate
+	def N1_rate(self):
 		return rates[1]
 	
 	@Gillespie.event(rates[2])
-	def increase_c(self):
-		self.c += 1
+	def increase_N2(self):
+		self.N[2] += 1
 	
 	@Gillespie.event(0)
 	def impossible_event(self):
 		raise AssertionError("This event should never happen")
-	
-	def state(self):
-		return self.a, self.b, self.c
 
-class MultiEventProcess(Gillespie):
-	def initialise(self):
-		self.N = [0,0,0]
-	
+class MultiEventProcess(ProcessBase):
 	@Gillespie.event(rates)
 	def increase(self,i):
 		if i<3:
 			self.N[i] += 1
 		else:
 			raise AssertionError("This event should never happen")
-	
-	def state(self):
-		return self.N
 
-class VariableRateMultiEventProcess(Gillespie):
-	def initialise(self):
-		self.N = [0,0,0]
-	
+class MultiEventProcess2D(ProcessBase):
+	@Gillespie.event(diag(rates))
+	def increase(self,i,j):
+		assert i==j
+		if i<3:
+			self.N[i] += 1
+		else:
+			raise AssertionError("This event should never happen")
+
+class VariableRateMultiEventProcess(ProcessBase):
 	@Gillespie.event
 	def increase(self,i):
 		if i<3:
@@ -69,14 +68,8 @@ class VariableRateMultiEventProcess(Gillespie):
 	@increase.rate
 	def increase_rate(self):
 		return rates
-	
-	def state(self):
-		return self.N
 
-class VariableRateMultiEventProcess2D(Gillespie):
-	def initialise(self):
-		self.N = [0,0,0]
-	
+class VariableRateMultiEventProcess2D(ProcessBase):
 	@Gillespie.event
 	def increase(self,i,j):
 		assert i==j
@@ -88,13 +81,11 @@ class VariableRateMultiEventProcess2D(Gillespie):
 	@increase.rate
 	def increase_rate(self):
 		return diag(rates)
-	
-	def state(self):
-		return self.N
 
 @mark.parametrize("Process",[
 		SimpleProcess,
 		MultiEventProcess,
+		MultiEventProcess2D,
 		VariableRateMultiEventProcess,
 		VariableRateMultiEventProcess2D,
 	])
